373521599cecdb5f51997ae713a337640f1e4585
misheck gogo <misheckgogo45@gmail.com>
2024-04-20 13:35:45 -0700

    Write a script that starts a Flask web application:HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A simple Flask web application with multiple routes"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+# Define a route for the root URL '/'
+@app.route('/', strict_slashes=False)
+def root():
+    """Display 'Hello HBNB!' when accessing the root URL"""
+    return "Hello HBNB!"
+
+# Define a route for '/hbnb'
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display 'HBNB' when accessing the '/hbnb' URL"""
+    return "HBNB"
+
+if __name__ == '__main__':
+    app.run(debug=True)
