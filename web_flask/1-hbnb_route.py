fed0271e90f827f0dd32307c50fb9f18c971f28c
misheck gogo <misheckgogo45@gmail.com>
2024-04-20 14:13:53 -0700

    commit

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""1-hbnb_route module"""
+"""A simple Flask web application with multiple routes"""
+
 from flask import Flask
 
 
 app = Flask(__name__)
 
 
+# Define a route for the root URL '/'
 @app.route('/', strict_slashes=False)
 def root():
     """/: display “Hello HBNB!”"""
     return "Hello HBNB!"
 
 
+# Define a route for '/hbnb'
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """/hbnb: display “HBNB”"""
