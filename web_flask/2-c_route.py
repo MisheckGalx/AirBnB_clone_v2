c9c3a60190a75f96eedf51fded8bf101937b3c8e
misheck gogo <misheckgogo45@gmail.com>
2024-04-20 14:46:01 -0700

    nn

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,14 +18,20 @@ def hbnb():
     return "HBNB"
 
 
+# Define a route for '/c/<text>'
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """/c/<text>: display C the text"""
-    text_written = "{}".format(text)
-    new_text_written = text_written.replace('_', ' ')
+    """
+    Display 'C <text>' where <text> is a URL parameter.
+    Replaces underscores with spaces in <text>.
+    """
+    # Replace underscores in <text> with spaces
+    formatted_text = text.replace('_', ' ')
 
-    return "C {}".format(new_text_written)
+    # Return the formatted text prefixed with 'C '
+    return f"C {formatted_text}"
 
-
+# Check if this script is being run directly by the Python interpreter
 if __name__ == '__main__':
+    # Run the Flask application in debug mode on the local development server
     app.run(debug=True)
