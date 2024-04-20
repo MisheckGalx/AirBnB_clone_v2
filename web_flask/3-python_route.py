6f71873fbe2000c3987c81c2f05f17113159cbf6
misheck gogo <misheckgogo45@gmail.com>
2024-04-20 15:16:46 -0700

    jj

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,42 +1,42 @@
 #!/usr/bin/python3
 """2-c_route module"""
-
 from flask import Flask
+
 
 app = Flask(__name__)
 
-# Route for the root URL '/'
-@app.route('/', strict_slashes=False)
-def root():
-    """Display 'Hello HBNB!' when accessing the root URL"""
-    return "Hello HBNB!"
 
-# Route for '/hbnb'
 @app.route('/', strict_slashes=False)
 def root():
     """/: display “Hello HBNB!”"""
     return "Hello HBNB!"
 
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """/hbnb: display “HBNB”"""
+    return "HBNB"
+
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """
-    'C <text>' where <text> is a URL parameter.
-    """
-    formatted_text = text.replace('_', ' ')
-    return f"C {formatted_text}"
+    """/c/<text>: display C and the text"""
+    text_written = "{}".format(text)
+    new_text_written = text_written.replace('_', ' ')
 
-# Routes for '/python', '/python/' and '/python/<text>'
+    return "C {}".format(new_text_written)
+
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text='is cool'):
-    """
-    'Python <text>' where <text> is a URL parameter (default is 'is cool').
-    """
+    """python_text: display Python and the text"""
+    text_written = "{}".format(text)
+    new_text_written = text_written.replace('_', ' ')
 
-    formatted_text = text.replace('_', ' ')
-    return f"Python {formatted_text}"
+    return "Python {}".format(new_text_written)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
