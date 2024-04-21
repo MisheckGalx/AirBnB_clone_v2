c30e5d4c2382fae5345fb0b1f6ad233b79c26f99
misheck gogo <misheckgogo45@gmail.com>
2024-04-21 15:11:24 -0700

    Write a script that starts a Flask web application:Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,6 +7,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# Define a route for the root URL '/' of the web application
 @app.route('/', strict_slashes=False)
 def display():
     """Starts a Flask web application"""
