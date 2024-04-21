48bf2efef4340289365db3a9b1f70c6ef801bd65
misheck gogo <misheckgogo45@gmail.com>
2024-04-21 15:01:59 -0700

    Write a script that starts a Flask web application:Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""0-hello_route module"""
+"""A simple Flask web application"""
+
 from flask import Flask
 
-
+# Create an instance of the Flask class
 app = Flask(__name__)
 
-
+# Define a route for the root URL '/' of the web application
 @app.route('/', strict_slashes=False)
 def display():
-    """Starts a Flask web application"""
+    """Display 'Hello HBNB!' when accessing the root URL"""
     return 'Hello HBNB!'
 
-
+# Check if this script is being run directly by the Python interpreter
 if __name__ == '__main__':
     app.run(debug=True)
